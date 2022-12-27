bbc25eb5656bb14dd7d4ca63f82f11ce235dcf32
Gonzalo <g.pernas@alumnos.upm.es>
2022-12-27 23:15:22 +0100

    arg fix & sockets

--- README.md
+++ README.md
@@ -11,9 +11,9 @@
 
 
 ## Info
-- In-Out events (Poll/Select/Kqueue): https://nima101.github.io/io_multiplexing 
 - TCP/IP Protocol in C++: https://lenngro.github.io/how-to/2021/01/05/Simple-TCPIP-Server-Cpp/
 - TCP/IP v4 example in C: https://www.ibm.com/docs/en/zos/2.1.0?topic=applications-example-ipv4-tcp-server-program
+- In-Out events (Poll/Select/Kqueue): https://nima101.github.io/io_multiplexing 
 - Non-Blocking fd C++: https://stackoverflow.com/questions/5616092/non-blocking-call-for-reading-descriptor
 - Non-Blocking fd C++ extended version: https://www.linuxtoday.com/blog/blocking-and-non-blocking-i-0/
 - Sockets: https://beej.us/guide/bgnet/pdf/bgnet_usl_c_1.pdf (Pag 60)
@@ -34,3 +34,5 @@
 - https://opensource.com/life/16/6/irc
 - https://oramind.com/tutorial-how-to-make-an-irc-server-connection/
 - https://se.inf.ethz.ch/student_projects/fabian_gremper/Report.pdf
+
+- TPC Server https://www.youtube.com/watch?v=cNdlrbZSkyQ
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpernas- <gpernas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gpernas- <gpernas-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 20:43:29 by gpernas-          #+#    #+#             */
-/*   Updated: 2022/12/22 22:01:50 by gpernas-         ###   ########.fr       */
+/*   Updated: 2022/12/27 23:11:15 by gpernas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void	saveArgs(std::string *args, int argc, char **argv) {
     for (int i = 1; i < argc; i++) {
         args[i - 1] = argv[i];
-    // 	std::cout << args[i - 1];
 	}
 }
 
@@ -29,9 +28,14 @@ void connectNetwork(std::string *args) {
 }
 
 int main(int argc, char **argv) {
-    std::string*	args = NULL;
+    std::string	args[argc - 1];
     saveArgs(args, argc, argv);
 	
+    // AF_INET ->       IPv4
+    // SOCK_STREAM ->   TCP con
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+
+    
     if (argc == 2)
         createNetwork(args);
     else if (argc == 4)
