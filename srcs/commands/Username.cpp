5479e6c69353e36ca0b6c867794f0f1cd94e4ca4
Kepa Aristi <kepaaristi@gmail.com>
2023-01-31 11:18:16 +0100

    refactor

--- srcs/commands/Username.cpp
+++ srcs/commands/Username.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Username.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gpernas- <gpernas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: karisti- <karisti-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 13:54:33 by gpernas-          #+#    #+#             */
-/*   Updated: 2023/01/26 21:45:16 by gpernas-         ###   ########.fr       */
+/*   Updated: 2023/01/31 10:35:04 by karisti-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,19 @@ IRC::Username::~Username()
 void IRC::Username::exec(IRC::Server* server, IRC::User& user)
 {
 	if (server->getPWD().size() > 0 && user.getPassword() != server->getPWD())
-		return ;	
+		return ;
 	std::vector<std::string> argSplit = splitString(this->args, " ", 3);
 	if (argSplit.size() < 4)
-		{
-			setError(ERR_NEEDMOREPARAMS, 1, this->command.c_str());
-			return ;
-		}
+		return setError(ERR_NEEDMOREPARAMS, 1, this->command.c_str());
 	for (std::vector<IRC::User>::iterator it = server->getUsers().begin(); it != server->getUsers().end(); it++)
 	{	
 		if ((argSplit.at(0)).compare(it.base()->getUser()) == 0)
-		{
-			setError(ERR_ALREADYREGISTRED, 0);
-			return ;
-		}
+			return setError(ERR_ALREADYREGISTRED, 0);
 	}
 	user.setUser(argSplit.at(0));
-	user.setHostname(argSplit.at(1)); 
-	user.setServername(argSplit.at(2)); 
+	user.setHostname(argSplit.at(1));
+	user.setServername(argSplit.at(2));
 	if (argSplit.at(3).at(0) == ':')
 		argSplit.at(3).erase(0, 1);
 	user.setRealname(argSplit.at(3));
-
 }
