fdd323699349a4a3d4e1c7a908a1d9efb1de51b1
Kepa Aristi <kepaaristi@gmail.com>
2023-02-14 17:36:30 +0100

    ping command done

--- srcs/commands/Pong.cpp
+++ srcs/commands/Pong.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Pong.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: karisti- <karisti-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: karisti- <karisti-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 19:05:45 by gpernas-          #+#    #+#             */
-/*   Updated: 2023/02/12 20:39:23 by karisti-         ###   ########.fr       */
+/*   Updated: 2023/02/14 17:14:40 by karisti-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,18 @@ IRC::Pong::~Pong() {}
 
 void IRC::Pong::exec(IRC::Server* server, IRC::User& user)
 {
-	(void) server;
-	if (args.size() > 0)
-	{
-		args = "PONG " + this->args;
-		user.sendMessage(args);
-	}
-		
+	std::string recepStr = splitString(this->args, " ")[0];
+
+	std::vector<IRC::User>::iterator receptor = findUser(server->getUsers(), recepStr);
+	if (receptor == server->getUsers().end())
+		return ;
+	
+	receptor->sendMessage("PING");
+
+	// user.setTimeout() = time(0);
+	(void)server;
+	(void)user;
+	
 	// if (user.getTimeout() > server->getTimeout())
 	//     // exit timeout
 	//     return ;
