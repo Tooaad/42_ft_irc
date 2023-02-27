4be47422ea4c36700b84488f93423254e4576c27
Kepa Aristi <kepaaristi@gmail.com>
2023-02-27 18:12:47 +0100

    Refactor: ping & pong

--- includes/commands/Pong.hpp
+++ includes/commands/Pong.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Pong.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: karisti- <karisti-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: karisti- <karisti-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 02:14:55 by gpernas-          #+#    #+#             */
-/*   Updated: 2023/02/07 12:01:18 by karisti-         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:12:24 by karisti-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@
 
 namespace IRC
 {
-	class Server;
-	class Command;
-	
 	class Pong : public Command
 	{
 		public:
