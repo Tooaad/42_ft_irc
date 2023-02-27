403665b517078402849803f10c00caa99d8b938e
Kepa Aristi <kepaaristi@gmail.com>
2023-02-27 17:23:39 +0100

    Refactor: names

--- includes/commands/ChannelNames.hpp
+++ includes/commands/ChannelNames.hpp
@@ -6,7 +6,7 @@
 /*   By: karisti- <karisti-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 19:10:06 by karisti-          #+#    #+#             */
-/*   Updated: 2023/02/24 18:32:35 by karisti-         ###   ########.fr       */
+/*   Updated: 2023/02/27 17:21:00 by karisti-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,13 @@ namespace IRC
 
 	class ChannelNames : public Command
 	{
-		private:
-			std::vector<Channel> channelsArray;
-
 		public:
 			ChannelNames();
-			ChannelNames(const ChannelNames& other);
 			virtual ~ChannelNames();
-			ChannelNames& operator=(const ChannelNames& other);
 
 			void	exec(Server* server, User& user);
 
 		private:
-			void	parseArgs(Server* server);
-			void	printNames(Server server, User user);
+			void	printNames(Server server, User user, std::vector<Channel>::iterator itBegin, std::vector<Channel>::iterator itEnd);
 	};
 }
--- srcs/commands/ChannelNames.cpp
+++ srcs/commands/ChannelNames.cpp
@@ -6,7 +6,7 @@
 /*   By: karisti- <karisti-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 19:10:20 by karisti-          #+#    #+#             */
-/*   Updated: 2023/02/24 18:42:15 by karisti-         ###   ########.fr       */
+/*   Updated: 2023/02/27 17:23:04 by karisti-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,17 @@
 
 
 IRC::ChannelNames::ChannelNames() {}
-IRC::ChannelNames::ChannelNames(const IRC::ChannelNames &other) { *this = other; }
 IRC::ChannelNames::~ChannelNames() {}
-
-IRC::ChannelNames &IRC::ChannelNames::operator=(const IRC::ChannelNames &other)
-{
-	if (this != &other)
-		channelsArray = other.channelsArray;
-	return *this;
-}
 
 void	IRC::ChannelNames::exec(IRC::Server* server, IRC::User& user)
 {
 	if (!user.isAuthenticated())
 		return setError(ERR_NOTREGISTERED, *server, user, 0);
 	
-	parseArgs(server);
-	
-	if (this->channelsArray.size() > 0)
-		printNames(*server, user);
-	else
-		setReply(RPL_ENDOFNAMES, *server, user, 1, "*");
-	
-	this->channelsArray.clear();
-}
-
-void	IRC::ChannelNames::parseArgs(IRC::Server* server)
-{
 	std::vector<std::string> argsArray = splitString(args, " ");
 	std::vector<std::string> channelsArrayStr = splitString(argsArray[0], ",");
-
+	std::vector<Channel> channelsArray;
+	
 	std::vector<Channel>::iterator channelIt;
 	for (std::vector<std::string>::iterator it = channelsArrayStr.begin(); it != channelsArrayStr.end(); it++)
 	{
@@ -51,20 +32,26 @@ void	IRC::ChannelNames::parseArgs(IRC::Server* server)
 			continue ;
 		channelIt = server->getChannelIt(*it);
 		if (channelIt == server->getChannels().end())
-			this->channelsArray.push_back(Channel(*it));
+			channelsArray.push_back(Channel(*it));
 		else
-			this->channelsArray.push_back(*channelIt);
+			channelsArray.push_back(*channelIt);
 	}
+	
+	if (channelsArray.size() > 0)
+		printNames(*server, user, channelsArray.begin(), channelsArray.end());
+	else
+		setReply(RPL_ENDOFNAMES, *server, user, 1, "*");
+	
+	// TODO: necesario ahora?
+	channelsArray.clear();
 }
 
-void	IRC::ChannelNames::printNames(IRC::Server server, IRC::User user)
+void	IRC::ChannelNames::printNames(IRC::Server server, IRC::User user, std::vector<IRC::Channel>::iterator itBegin, std::vector<IRC::Channel>::iterator itEnd)
 {
-	for (std::vector<Channel>::iterator it = this->channelsArray.begin(); it != this->channelsArray.end(); ++it)
+	for (std::vector<Channel>::iterator it = itBegin; it != itEnd; ++it)
 	{
 		if (it->isSecret() && !user.isInChannel(*it))
-		{
 			continue ;
-		}
 
 		if (it->getUsers().size() > 0)
 		{
