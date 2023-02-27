15da9d33076f500030a8bedd9a106d9dce479217
Kepa Aristi <kepaaristi@gmail.com>
2023-02-27 16:47:46 +0100

    Refactor: topic

--- includes/commands/ChannelTopic.hpp
+++ includes/commands/ChannelTopic.hpp
@@ -3,24 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   ChannelTopic.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: karisti- <karisti-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: karisti- <karisti-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 18:29:32 by gpernas-          #+#    #+#             */
-/*   Updated: 2023/02/07 12:00:57 by karisti-         ###   ########.fr       */
+/*   Updated: 2023/02/27 16:46:23 by karisti-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "../Command.hpp"
-#include <algorithm>
 
 
 namespace IRC
 {
-	class Server;
-	class Command;
-
 	class ChannelTopic : public Command
 	{
 		public:
