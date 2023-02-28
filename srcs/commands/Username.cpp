974da5d729c4f6acec330d8c2f437b4ae23f14a2
Gonzalo <gonper@yahoo.es>
2023-02-28 15:52:28 +0100

    Username ':' fix

--- srcs/commands/Username.cpp
+++ srcs/commands/Username.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Username.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: karisti- <karisti-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gpernas- <gpernas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 13:54:33 by gpernas-          #+#    #+#             */
-/*   Updated: 2023/02/28 12:40:27 by karisti-         ###   ########.fr       */
+/*   Updated: 2023/02/28 15:03:05 by gpernas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,15 @@ void	IRC::Username::exec(IRC::Server* server, IRC::User& user)
 		if ((argSplit.at(0)).compare(it.base()->getUser()) == 0)
 			return setError(ERR_ALREADYREGISTRED, *server, user, 0);
 	}
+
 	user.setUser(argSplit.at(0));
-	// user.setHostname(argSplit.at(1)); TODO: esto?
+	// user.setHostname(argSplit.at(1));
 	user.setServername(argSplit.at(2));
 	if (argSplit.at(3).at(0) == ':')
+	{
 		argSplit.at(3).erase(0, 1);
-	user.setRealname(argSplit.at(3));
+		user.setRealname(argSplit.at(3));
+		return ;
+	}
+	user.setRealname(splitString(argSplit.at(3), " ", 1)[0]);
 }
